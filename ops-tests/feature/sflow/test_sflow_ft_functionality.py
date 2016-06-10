e0685189c146a01c29e769851e6e0f047c786da1
noorinshaikh <noorinbano.shaikh@hpe.com>
2016-06-10 16:24:54 -0700

    Added ipv6 ping
    TG-1378
    Tags: fix, dev
    Fix to check ipv6 supportablity
    
    Change-Id: Ied49b451ceab3c39997d25fc871d3c3b5accc2fb
    Signed-off-by: noorinshaikh <noorinbano.shaikh@hpe.com>

--- ops-tests/feature/sflow/test_sflow_ft_functionality.py
+++ ops-tests/feature/sflow/test_sflow_ft_functionality.py
@@ -118,7 +118,7 @@ def test_sflow_ft_functionality(topology, step):
     with ops1.libs.vtysh.Configure() as ctx:
         ctx.sflow_enable()
         ctx.sflow_sampling(sampling_rate)
-        ctx.sflow_agent_interface(p3)
+        ctx.sflow_agent_interface(p3, address_family='ipv4')
         ctx.sflow_collector('10.10.12.2')
 
     ops1('show sflow')
@@ -153,7 +153,8 @@ def test_sflow_ft_functionality(topology, step):
     assert sflow_utils.check_ping_sample(sflow_output=result,
                                          host1='10.10.10.2',
                                          host2='10.10.10.1',
-                                         agent_address='10.10.12.1')
+                                         agent_address='10.10.12.1',
+                                         family='ipv4')
 
     # Generate L3 traffic
     step("### Checking Routed traffic (L3) ###")
@@ -162,10 +163,13 @@ def test_sflow_ft_functionality(topology, step):
     result = hs3.libs.sflowtool.stop()
 
     assert len(result['packets']) > 0
+
+    # Check for ping request and response packets
     assert sflow_utils.check_ping_sample(sflow_output=result,
                                          host1='10.10.10.2',
                                          host2='10.10.11.2',
-                                         agent_address='10.10.12.1')
+                                         agent_address='10.10.12.1',
+                                         family='ipv4')
 
     step("### Disabling sFlow ###")
     with ops1.libs.vtysh.Configure() as ctx:
@@ -224,4 +228,97 @@ def test_sflow_ft_functionality(topology, step):
     assert sflow_utils.check_ping_sample(sflow_output=result,
                                          host1='10.10.10.2',
                                          host2='10.10.10.3',
-                                         agent_address='10.10.12.1')
+                                         agent_address='10.10.12.1',
+                                         family='ipv4')
+
+    # Configure host ipv6 interfaces
+    step("### Configuring host ipv6 interfaces ###")
+    hs1.libs.ip.interface('1', addr='1000::2/64', up=True)
+    hs2.libs.ip.interface('1', addr='1001::2/64', up=True)
+    hs3.libs.ip.interface('1', addr='1002::2/64', up=True)
+
+    # Add ipv6 routes on hosts
+    step("### Adding ipv6 routes on hosts ###")
+    hs1.libs.ip.add_route('1001::/64', '1000::1')
+    hs2.libs.ip.add_route('1000::/64', '1001::1')
+
+    # Configure ipv6 interfaces on the switch
+    step("Configuring ipv6 interface 1 of switch")
+    with ops1.libs.vtysh.ConfigInterface('1') as ctx:
+        ctx.routing()
+        ctx.ipv6_address('1000::1/64')
+        ctx.no_shutdown()
+
+    step("Configuring ipv6 interface 2 of switch")
+    with ops1.libs.vtysh.ConfigInterface('2') as ctx:
+        ctx.routing()
+        ctx.ipv6_address('1001::1/64')
+        ctx.no_shutdown()
+
+    step("Configuring ipv6 interface 3 of switch")
+    with ops1.libs.vtysh.ConfigInterface('3') as ctx:
+        ctx.no_ip_address('10.10.12.1/24')
+        ctx.ipv6_address('1002::1/64')
+        ctx.no_shutdown()
+
+    # Configure sFlow
+    step("### Configuring sFlow ###")
+    with ops1.libs.vtysh.Configure() as ctx:
+        ctx.sflow_agent_interface(p3, address_family='ipv6')
+        ctx.no_sflow_enable()
+        ctx.sflow_enable()
+        ctx.sflow_collector('1002::2')
+
+    ops1('show sflow')
+
+    collector = {}
+    collector['ip'] = '1002::2'
+    collector['port'] = '6343'
+    collector['vrf'] = 'vrf_default'
+
+    sflow_config = ops1.libs.vtysh.show_sflow()
+    assert sflow_config['sflow'] == 'enabled'
+    assert int(sflow_config['sampling_rate']) == sampling_rate
+    assert sflow_config['collector'][1] == collector
+    assert str(sflow_config['agent_interface']) == p3
+
+    time.sleep(20)
+
+    # Start sflowtool
+    hs3.libs.sflowtool.start(mode='line')
+
+    # Generate CPU destined traffic
+    step("### Checking CPU destined traffic ###")
+    hs1.libs.ping.ping(ping_count, '1000::1', ping_interval)
+
+    # Stop sflowtool
+    result = hs3.libs.sflowtool.stop()
+
+    # Checking if packets are captured by collector
+    assert len(result['packets']) > 0
+
+    # Check for ping request and response packets
+    assert sflow_utils.check_ping_sample(sflow_output=result,
+                                         host1='1000:0000:0000:0000:'
+                                         '0000:0000:0000:0002',
+                                         host2='1000:0000:0000:0000:'
+                                         '0000:0000:0000:0001',
+                                         agent_address='1002:0000:0000:0000:'
+                                         '0000:0000:0000:0001', family='ipv6')
+
+    # Generate L3 traffic
+    step("### Checking Routed traffic (L3) ###")
+    hs3.libs.sflowtool.start(mode='line')
+    hs1.libs.ping.ping(ping_count, '1001::2', ping_interval)
+    result = hs3.libs.sflowtool.stop()
+
+    assert len(result['packets']) > 0
+
+    # Check for ping request and response packets
+    assert sflow_utils.check_ping_sample(sflow_output=result,
+                                         host1='1000:0000:0000:0000:'
+                                         '0000:0000:0000:0002',
+                                         host2='1001:0000:0000:0000:'
+                                         '0000:0000:0000:0002',
+                                         agent_address='1002:0000:0000:0000:'
+                                         '0000:0000:0000:0001', family='ipv6')

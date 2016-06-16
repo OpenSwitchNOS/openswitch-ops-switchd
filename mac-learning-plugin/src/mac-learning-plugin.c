66bfff44189ce8eb76637978226083cc119c61ad
Harshvardhan <harshvardhan@hpe.com>
2016-06-16 11:30:59 -0700

    ops-utils call for setting and getting tag and trunked VLANs for port table
    Tags: fix, dev
    TG-1175
    
    Change-Id: Ie0f4a997e32167d08a2e493e4ea295acc10a1536
    Signed-off-by: Harshvardhan <harshvardhan@hpe.com>

--- mac-learning-plugin/src/mac-learning-plugin.c
+++ mac-learning-plugin/src/mac-learning-plugin.c
@@ -170,6 +170,7 @@ static void mac_learning_table_monitor (struct blk_params *blk_params)
         ovsdb_idl_omit_alert(idl, &ovsrec_mac_col_bridge);
         ovsdb_idl_omit_alert(idl, &ovsrec_mac_col_from);
         ovsdb_idl_omit_alert(idl, &ovsrec_mac_col_vlan);
+        ovsdb_idl_omit_alert(idl, &ovsrec_mac_col_mac_vlan);
         ovsdb_idl_omit_alert(idl, &ovsrec_mac_col_mac_addr);
         ovsdb_idl_omit_alert(idl, &ovsrec_mac_col_tunnel_key);
         ovsdb_idl_omit_alert(idl, &ovsrec_mac_col_port);
@@ -186,6 +187,7 @@ static void mac_learning_table_monitor (struct blk_params *blk_params)
     ovsdb_idl_index_add_column(index, &ovsrec_mac_col_mac_addr,
                                OVSDB_INDEX_ASC, ovsrec_mac_index_mac_addr_cmp);
     ovsdb_idl_index_add_column(index, &ovsrec_mac_col_vlan, OVSDB_INDEX_ASC, NULL);
+    ovsdb_idl_index_add_column(index, &ovsrec_mac_col_mac_vlan, OVSDB_INDEX_ASC, NULL);
     ovsdb_idl_index_add_column(index, &ovsrec_mac_col_from,
                                OVSDB_INDEX_ASC, ovsrec_mac_index_from_cmp);
 
@@ -252,7 +254,7 @@ mlearn_plugin_db_add_local_mac_entry (
     ovsrec_mac_set_from(mac_e, OVSREC_MAC_FROM_DYNAMIC);
     ovsrec_mac_set_mac_addr(mac_e, str);
     ovsrec_mac_set_port(mac_e, port->cfg);
-    ovsrec_mac_set_vlan(mac_e, mlearn_node->vlan);
+    ops_mac_set_vlan(mlearn_node->vlan, mac_e, idl);
 
 }
 
@@ -284,6 +286,7 @@ mlearn_plugin_db_del_local_mac_entry (struct mlearn_hmap_node *mlearn_node)
     /* initialize the indexes with values to be comapred  */
     mac_val.mac_addr = str;
     mac_val.vlan = mlearn_node->vlan;
+    ops_mac_set_vlan(mlearn_node->vlan, &mac_val, idl);
     mac_val.from = OVSREC_MAC_FROM_DYNAMIC;
 
     OVSREC_MAC_FOR_EACH_EQUAL (mac_e, &cursor, &mac_val) {

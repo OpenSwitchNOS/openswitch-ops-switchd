55959e150554e9357c8e059b372f8ff9858d7b22
Chinh Le <chinh.le@hpe.com>
2016-09-12 14:50:51 -0700

    Refactor tunneling CLIs and add initial GRE CLIs
    Change-Id: Ib208801eae9ed16eae207496636cd2ed82c9ac88
    Signed-off-by: Chinh Le <chinh.le@hpe.com>

--- include/gre_tunnel_vty.h
+++ include/gre_tunnel_vty.h
@@ -1,4 +1,4 @@
-/* TUNNEL CLI commands
+/* GRE tunnel CLI commands
  *
  * Copyright (C) 2016 Hewlett Packard Enterprise Development LP
  *
@@ -16,31 +16,19 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
  * 02110-1301, USA.
- *
- * File: tunnel_vty.h
- *
- * Purpose:  To add TUNNEL CLI configuration and display commands.
  */
-#ifndef _TUNNEL_VTY_H
-#define _TUNNEL_VTY_H
-
-#define MAX_TUNNEL_LENGTH   15
-#define MAX_INTF_LENGTH     15
-#define MAX_VLAN_LENGTH     15
+#ifndef _GRE_TUNNEL_VTY_H
+#define _GRE_TUNNEL_VTY_H
 
 /* Help strings */
-#define TUNNEL_HELP_STR "Create a tunnel interface\n"
 #define TUNNEL_NUM_HELP_STR "Tunnel number\n"
 #define TUNNEL_MODE_HELP_STR "Select a tunnel mode\n"
 #define TUNNEL_MODE_OPTS_HELP_STR "Tunnel mode for the interface\n"
-#define IPV4_HELP_STR "IPv4 information\n"
 
 /* Constants */
 #define TUNNEL_MODE_GRE_STR "gre"
 #define TUNNEL_IPV4_TYPE_STR "ipv4"
-#define TUNNEL_GRE_IPV4_STR "gre_ipv4"
 
-void cli_post_init(void);
-void cli_pre_init(void);
+void gre_tunnel_add_clis(void);
 
 #endif
